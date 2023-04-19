9f268dcaf21ada5f76408eb2fced33d1895b01cb
Marsellino10 <marsellinomedhat10@gmail.com>
2023-04-19 16:38:36 +0200

    nn

--- backend/controllers/login.controller.js
+++ backend/controllers/login.controller.js
@@ -1,30 +1,28 @@
 const user = require("../models/user.model");
-const { dbConnect, dbDisconnect } = require("../utils/db.util");
 const bcrypt = require("bcrypt");
-const bp = require("body-parser").urlencoded({ extended: true });
-const request = require("request-promise");
 const jwt = require("jsonwebtoken");
 
 exports.login = async (req, res) => {
-  await dbConnect();
   let email = req.body.email;
   let password = req.body.password;
-  console.log(req.body);
-  let hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  // console.log(email,password,hash)
-  let userData = await user.findOne({ email: email });
-  console.log(userData);
-  if (userData !== null) {
-    if (await bcrypt.compare(password, userData.password)) {
-      const token = jwt.sign({ data: userData }, process.env.JWT_SECRET_KEY, {
+  let x = await user.findOne({ email: email });
+  if (x !== null) {
+    if (await bcrypt.compare(password, x.password)) {
+      const token = jwt.sign({ id: x._id }, process.env.JWT_SECRET_KEY, {
         expiresIn: process.env.JWT_EXPIRATION_TIME,
       });
-      res.status(200).json({userData , token});
+      // res.send(x._id);
+      res.status(201).send(token);
     } else {
-      res.status(400).send("Wrong Password");
+      res.status(200).send("Wrong Password");
     }
   } else {
-    res.status(400).send("Wrong email");
+    res.status(200).send("Wrong email");
   }
-  dbDisconnect();
+};
+
+exports.check = async (req, res) => {
+  if (req.user != null) {
+    res.status(200).send("already logged in");
+  }
 };
--- backend/controllers/signup.controller.js
+++ backend/controllers/signup.controller.js
@@ -1,35 +1,29 @@
 const user = require("../models/user.model");
-const db = require("../utils/db.util");
 const users = require("../models/user.model");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
 
 exports.addUser = async (req, res) => {
   if (Object.keys(req.body).length == 4) {
-    await db.dbConnect();
     const hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
-    const first = req.body.firstName;
-    const last = req.body.lastName;
-    const errorr = "Email is already exist";
     let data = {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
       email: req.body.email,
       password: hash,
     };
-    const token = jwt.sign({ data: data }, process.env.JWT_SECRET_KEY, {
-      expiresIn: process.env.JWT_EXPIRATION_TIME,
-    });
-    
-    if (!(await db.checkData(users, "email", req.body.email)) === true) {
-      res.status(400).json({errorr});
+    if (!((await user.findOne({ email: req.body.email })) === null)) {
+      res.status(500).send("failed");
     } else {
       await users.create(data);
-      res.status(200).json({first,last});
+      res.status(200).send("Done");
     }
-    db.dbDisconnect();
   } else {
     res.status(500).send("Error");
+  }
+};
+
+exports.check = (req, res) => {
+  if (req.user != null) {
+    res.status(404).send("Cant signup when logged in");
   }
 };
